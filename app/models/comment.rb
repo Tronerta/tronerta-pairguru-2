2c54323d537cd7c7c474f27e9a99f5de035646c2
tronerta <tronerta.web@gmail.com>
2019-12-18 14:05:05 +0100

    Top commenters page setup

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :movie
   belongs_to :user
 
-  validates_presence_of :user_id
-  validates_presence_of :movie_id
+  validates :user_id, presence: true
+  validates :movie_id, presence: true
   validates :movie_id, uniqueness: { scope: :user_id, message: "You've already commented this movie!" }
+
+  scope :recents, -> {
+    where("comments.created_at >= ?", Time.zone.now - 7.days)
+  }
 end
