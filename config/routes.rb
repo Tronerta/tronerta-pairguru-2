7edc9871147dbd6d83de2b0da6eec0708d34eecb
tronerta <tronerta.web@gmail.com>
2019-12-18 13:18:16 +0100

    API V2 setup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :movies, only: [:index, :show]
     end
+    namespace :v2 do
+      resources :movies, only: [:index, :show]
+    end
   end
 
   root "home#welcome"
